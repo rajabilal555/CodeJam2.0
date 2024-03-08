c17484a3e98000bcec557dc7cd8f55b182333df2
Bilal Raja <rajabilal555@gmail.com>
2024-03-08 16:35:13 +0500

    feat: add firebase and project structure

---
+++ lib/utilities/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final darkThemeData = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFFFF00)),
+  brightness: Brightness.dark,
+  useMaterial3: true,
+);
